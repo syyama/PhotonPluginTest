d2e1a6715868de64c1d8af84e4a82fb05eedd1be
syyama <syyama@syyama.net>
2016-04-17 16:29:31 +0900

    First Commit

---
+++ PluginTest/PluginFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Photon.Hive.Plugin.PluginTest
+{
+    class PluginFactory : IPluginFactory
+    {
+        public IGamePlugin Create(IPluginHost gameHost, string pluginName, Dictionary<string, string> config, out string errorMsg)
+        {
+            var plugin = new TestPlugin();
+            if (plugin.SetupInstance(gameHost, config, out errorMsg))
+            {
+                return plugin;
+            }
+            return null;
+        }
+    }
+}
---
+++ PluginTest/TestPlugin.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Net.Http;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Photon.Hive.Plugin.PluginTest
+{
+
+    public class TestPlugin : PluginBase
+    {
+        public TestPlugin()
+        {
+            this.UseStrictMode = true;
+        }
+
+        public override void OnSetProperties(ISetPropertiesCallInfo info)
+        {
+            base.OnSetProperties(info);
+
+            var dict = new Dictionary<string, string>();
+
+            dict["player_name"] = info.Nickname;
+
+            foreach (DictionaryEntry entry in info.Request.Properties)
+            {
+                dict[entry.Key.ToString()] = entry.Value.ToString();
+            }
+
+            MainProc(dict).Wait();
+        }
+
+        internal static async Task MainProc(Dictionary<string, string> dict)
+        {
+            var result = await Post("http://photon-test-app.herokuapp.com/", dict);
+        }
+
+        private static async Task<string> Post(string url, Dictionary<string, string> dict)
+        {
+            string result = "";
+
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                httpClient.MaxResponseContentBufferSize = int.MaxValue;
+
+                HttpContent content = new FormUrlEncodedContent(dict);
+                var resopnse = await httpClient.PostAsync(url, content);
+                var text = await resopnse.Content.ReadAsStringAsync();
+
+                result = text;
+            }
+            catch (Exception e)
+            {
+                result= e.Message;
+            }
+            return result;
+        }
+    }
+}
